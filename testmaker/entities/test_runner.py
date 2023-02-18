efa8be5cd4595c15d65d5ac5c8d8213528163734
Nguyễn Anh Bình <sometimesocrazy@gmail.com>
2023-02-18 15:16:04 +0700

    Fix bug format

--- testmaker/entities/test_runner.py
+++ testmaker/entities/test_runner.py
@@ -24,28 +24,35 @@ class TestRunner:
         return self.loader.load_test_case(testcase_num)
 
     def get_test_folders(self):
-        folders=  glob.glob(f"{self.testcase_folder}/*__*")
-        folders = [folder for folder in folders if os.path.basename(folder).split("__")[0] == self.function_name]
+        folders = glob.glob(f"{self.testcase_folder}/*__*")
+        folders = [
+            folder
+            for folder in folders
+            if os.path.basename(folder).split("__")[0] == self.function_name
+        ]
         return folders
+
     def __test_case_generator(self):
         test_folders = self.get_test_folders()
         for folder in test_folders:
             test_num = folder.split("__")[-1]
             test_case = self.load_test_case(test_num)
-            yield test_num,*test_case
-    def load_all_test_cases(self):
-        test_case_count  = len(self.get_test_folders())
-        return test_case_count,self.__test_case_generator()
+            yield test_num, *test_case
 
+    def load_all_test_cases(self):
+        test_case_count = len(self.get_test_folders())
+        return test_case_count, self.__test_case_generator()
 
     def run_all_test_cases(self):
         # If no testcase_num is provided, run all test cases
         test_case_count, test_cases_generator = self.load_all_test_cases()
         logging.info(f"Running {test_case_count} test cases")
-        progress_bar = tqdm(test_cases_generator,total=test_case_count,desc="Running test cases")
-        for test_index,inputs, results in progress_bar:
+        progress_bar = tqdm(
+            test_cases_generator, total=test_case_count, desc="Running test cases"
+        )
+        for test_index, inputs, results in progress_bar:
             self.run_test_case(inputs, results)
-            message=  f"Test case {test_index} passed"
+            message = f"Test case {test_index} passed"
             logging.info(message)
             progress_bar.set_description(message)
 
